b10e6537d743cc3011338b823e1993c6015d8f89
Rohitt211 <rohitPtechdev211@gmail.com>
2024-09-04 16:56:53 +0530

    Fix 404 issue with GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import ArticlePage from './components/ArticlePage';
 import { useParams } from 'react-router-dom';
