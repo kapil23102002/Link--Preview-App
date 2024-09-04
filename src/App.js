1e3475d215f3d961121982359ae4f5a3fefcda7a
Rohitt211 <rohitPtechdev211@gmail.com>
2024-09-04 18:11:05 +0530

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "change""""""""
    This reverts commit 5d13fd501426eab2182970c45ef2c0c42d032d47.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Article1 from "./components/ArticlePage1";
+import Article2 from "./components/ArticlePage2";
+import Article3 from "./components/ArticlePage3";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/article1" element={<Article1 />} />
+          <Route path="/article2" element={<Article2 />} />
+          <Route path="/article3" element={<Article3 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
